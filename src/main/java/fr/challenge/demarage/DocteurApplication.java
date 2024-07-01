885ad5fbfa6a249da660ddad235e403ff20b590e
StanislasComputerScience <remilabonne@yahoo.fr>
2024-07-01 15:10:26 +0200

    on envoie le chalenge

---
+++ src/main/java/fr/challenge/demarage/DocteurApplication.java
@@ -0,0 +1,13 @@
+package fr.challenge.demarage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DocteurApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DocteurApplication.class, args);
+	}
+
+}
